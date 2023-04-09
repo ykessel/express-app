26a47af6952e80c8bd503e1b28495901ead6f5f5
yanniel kessel <kesselyanniel@gmail.com>
2023-04-08 23:53:59 -0400

    test with my own cluster

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 import express, { json } from "express";
 import cors from "cors";
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config()
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
-// import { connectToServer } from "./db/connection.js";
 import { createUserRoute } from "./routes/user.route.js";
 
 app.use(cors());
 app.use(json());
-app.use('/users', createUserRoute);
- 
+app.use("/users", createUserRoute);
+
 app.listen(port, () => {
-    mongoose.connect(process.env.MONGO_URI_MINE, {
+  mongoose
+    .connect(process.env.MONGO_URI_MINE, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then(() => console.log("Connected to the database"))
-    .catch((err) => console.error(err))
+    .catch((err) => console.error("Mongoose Error --> ", err));
 
   console.log(`Server is running on port: ${port}`);
 });
