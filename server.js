797fe65b172c2dd863f990bc5e7178eddc37cd35
yanniel kessel <kesselyanniel@gmail.com>
2023-04-09 01:08:44 -0400

    test with my own cluster

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ app.use("/users", createUserRoute);
 
 app.listen(port, () => {
   mongoose
-    .connect(process.env.MONGO_URI_MINE, {
+    .connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log("Connected to the database"))
-    .catch((err) => console.error("Mongoose Error --> ", err));
+    .then(() =>
+      console.log("Connected to the database: ", process.env.MONGO_URI)
+    )
+    .catch((err) => console.error("Mongoose Error: ", err));
 
   console.log(`Server is running on port: ${port}`);
 });
